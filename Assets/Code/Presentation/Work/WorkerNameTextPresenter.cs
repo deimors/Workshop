a8cd68a1e31136ab643ab94488006c8f4bd5b3c1
deimors <deimors@gmail.com>
2017-12-31 13:24:55 -0600

    Worker Name Text

---
+++ Assets/Code/Presentation/Work/WorkerNameTextPresenter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Workshop.Domain.Work;
+using Zenject;
+
+namespace Workshop.Presentation.Work
+{
+	public class WorkerNameTextPresenter : MonoBehaviour
+	{
+		[SerializeField]
+		private Text _workerNameText;
+
+		[Inject]
+		public void Setup(WorkerIdentifier worker)
+			=> _workerNameText.text = worker.ToString();
+	}
+}
