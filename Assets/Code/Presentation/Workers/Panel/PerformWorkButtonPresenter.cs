6c89e89fcd5e499bbe112f202d711e16ff6fcd56
deimors <deimors@gmail.com>
2018-01-10 18:26:37 -0600

    PerformWork button non-interactable when Worker busy

--- Assets/Code/Presentation/Workers/Panel/PerformWorkButtonPresenter.cs
+++ Assets/Code/Presentation/Workers/Panel/PerformWorkButtonPresenter.cs
@@ -1,7 +1,9 @@
-﻿using UniRx;
+﻿using System;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 using Workshop.Domain.Work;
+using Workshop.Domain.Work.Aggregates;
 using Workshop.UseCases.Work;
 using Zenject;
 
@@ -10,12 +12,21 @@ namespace Workshop.Presentation.Workers.Panel
 	public class PerformWorkButtonPresenter : MonoBehaviour
 	{
 		[SerializeField]
-		private Button StartButton;
+		private Button _performWorkButton;
 		
 		[Inject]
-		public void Initialize(WorkerIdentifier worker, IPerformAssignedWork performAssignedWork)
+		public void Initialize(WorkerIdentifier workerId, IPerformAssignedWork performAssignedWork, IObservable<WorkshopEvent> workshopEvents)
 		{
-			StartButton.onClick.AsObservable().Subscribe(_ => performAssignedWork.Perform(worker));
+			_performWorkButton
+				.onClick
+				.AsObservable()
+				.Subscribe(_ => performAssignedWork.Perform(workerId));
+
+			workshopEvents
+				.OfType<WorkshopEvent, WorkshopEvent.WorkerStatusUpdated>()
+				.Where(workerStatusUpdated => workerStatusUpdated.WorkerId == workerId)
+				.Select(workerStatusUpdated => workerStatusUpdated.NewStatus.Busy)
+				.Subscribe(busyStatus => _performWorkButton.interactable = !busyStatus);
 		}
-	}
+	}	
 }
