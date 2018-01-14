c32005379dc39f4b831794a3d6d93579aa1d3af6
deimors <deimors@gmail.com>
2018-01-14 15:38:00 -0600

    JobListDropdown Validation

---
+++ Assets/Code/Presentation/Workers/Panel/JobListDropdownValidationPresenter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Workshop.Domain.Work.Aggregates;
+using Workshop.UseCases.Work;
+
+namespace Workshop.Presentation.Workers.Panel
+{
+	public class JobListDropdownValidationPresenter : MonoBehaviour, IDisplayJobListDropdownValidation
+	{
+		[SerializeField]
+		private Image _image;
+
+		public void ShowError(WorkshopError error)
+		{
+			var original = _image.color;
+
+			_image.color = Color.red;
+
+			Observable.Timer(TimeSpan.FromSeconds(.5))
+				.Subscribe(_ => _image.color = original);
+		}
+	}
+}
--- Assets/Code/UseCases/Workers/AssignOrUnassignJobToWorkerWhenJobSelected.cs
+++ Assets/Code/UseCases/Workers/AssignOrUnassignJobToWorkerWhenJobSelected.cs
@@ -6,20 +6,25 @@ using Workshop.Domain.Work.Aggregates;
 
 namespace Workshop.UseCases.Work
 {
+	public interface IDisplayJobListDropdownValidation
+	{
+		void ShowError(WorkshopError error);
+	}
+
 	public class AssignOrUnassignJobToWorkerWhenJobSelected
 	{
-		public AssignOrUnassignJobToWorkerWhenJobSelected(WorkerIdentifier workerId, IJobSelectedObservable jobSelectedObservable, IEnqueueCommand<WorkshopCommand> workshopCommands)
+		public AssignOrUnassignJobToWorkerWhenJobSelected(WorkerIdentifier workerId, IJobSelectedObservable jobSelectedObservable, IEnqueueCommand<WorkshopCommand, WorkshopError> workshopCommands, IDisplayJobListDropdownValidation displayValidation)
 		{
 			jobSelectedObservable
 				.Select(maybeJobId => BuildJobSelectionCommand(maybeJobId, workerId))
-				.Subscribe(workshopCommands.Enqueue);
+				.SelectMany(command => workshopCommands.Enqueue(command))
+				.Subscribe(result => result.Switch(success => { }, failure => displayValidation.ShowError(failure.Error)));
 		}
-
+		
 		private WorkshopCommand BuildJobSelectionCommand(Maybe<JobIdentifier> maybeJobId, WorkerIdentifier workerId)
 			=> maybeJobId.SelectOrElse<JobIdentifier, WorkshopCommand>(
 				jobId => new WorkshopCommand.AssignJob(jobId, workerId),
 				() => new WorkshopCommand.UnassignWorker(workerId)
 			);
 	}
-
 }
